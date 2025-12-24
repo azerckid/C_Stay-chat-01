3f2821a73c5a42f22bef1b9b8e93df56577dd3c4
azerckid <azerckid@gmail.com>
2025-12-25 00:53:50 +0900

    fix: 잘못된 Vercel Preset 설정 제거

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from "@react-router/dev/config";
-import { vercelPreset } from "@react-router/dev/presets";
 
 export default {
-  // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  presets: [vercelPreset()],
 } satisfies Config;
